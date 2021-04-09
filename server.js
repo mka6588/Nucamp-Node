c02dc65d6ae25d58c8d1274322c236a7b9573869
Marvin A <mka507@gmail.com>
2021-04-09 18:57:34 -0400

    intro to Express Part 2

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 //use method can take a callback function w/c is a special type of function that express calls a middleware function. A middleware function in express has access to 3 parameters
 //req(request object), res(response object) and next(a function) 
 app.use((req, res) => {
-    console.log(req.headers);
+    // console.log(req.headers); morgan will now handle logging the request in now
     res.statusCode = 200;
     res.setHeader('Content-Tyoe', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
