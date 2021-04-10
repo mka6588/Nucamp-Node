4991ad0300831f5189dec41bf712e30c818129d2
Marvin A <mka507@gmail.com>
2021-04-09 21:32:27 -0400

    Express Simple REST

--- server.js
+++ server.js
@@ -5,10 +5,55 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json()); // when the server receives requests with json formatted data in the body this middleware will handle 
+//parsing that json data into js properties of the request object to be used in js
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Context-Type', 'text/plain');
+    next(); //pass control of the app routing to the next relevant routing method or this will stop here
+});
+
+app.get('/campsites', (req, res) => {              // response status code and headers already set in the app.all
+    res.end('Will send all the campsites to you'); // sends a message body back to client
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on  /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => { // what happens if you dont do res.end?
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} 
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
 
 app.use(express.static(__dirname + '/public'));
 
-//use method can take a callback function w/c is a special type of function that express calls a middleware function. A middleware function in express has access to 3 parameters
+//use method can take a callback middleware function w/c is a special type of function that express calls a middleware function. A middleware function in express has access to 3 parameters
 //req(request object), res(response object) and next(a function) 
 app.use((req, res) => {
     // console.log(req.headers); morgan will now handle logging the request in now
