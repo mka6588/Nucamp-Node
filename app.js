dda344b2970a702462c6f145d34b446ddc64fbc4
Marvin A <mka507@gmail.com>
2021-04-23 11:46:38 -0400

    Passport JWT

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
 const passport = require('passport');
-const authenticate = require('./authenticate');
+// const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +17,8 @@ const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+// const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -39,17 +41,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); can conflict with express session implementation of cookies
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false, //when a new session is created but then no update made to it , at the end of the request it wont get save,
-  // bec it would just be an empty session and no cookie is sent to clien to prevent saving a bunch of empty sessions.
-  resave: false,
-  store: new FileStore()
-}));
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false, //when a new session is created but then no update made to it , at the end of the request it wont get save,
+//   // bec it would just be an empty session and no cookie is sent to clien to prevent saving a bunch of empty sessions.
+//   resave: false,
+//   store: new FileStore()
+// }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -91,20 +93,20 @@ app.use('/users', usersRouter);
 //   }
 // }
 
-function auth(req, res, next) {
-  console.log(req.session);
-  console.log(req.user);
+// function auth(req, res, next) {
+//   console.log(req.session);
+//   console.log(req.user);
 
-  if (!req.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
+//   if (!req.user) {
+//     const err = new Error('You are not authenticated!');
+//     err.status = 401;
+//     return next(err);
+//   } else {
+//     return next();
+//   }
+// }
 
-app.use(auth);
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
+const authenticate = require('../authenticate');
 
 
 const router = express.Router();
@@ -28,33 +29,14 @@ router.post('/signup', (req, res) => {
       }
     }
   );
-  // User.findOne({ username: req.body.username })
-  //   .then(user => {
-  //     if (user) {
-  //       const err = new Error(`User ${req.body.username} already exists!`);
-  //       err.status = 403;
-  //       return next(err);
-  //     } else {
-  //       User.create({
-  //         username: req.body.username,
-  //         password: req.body.password
-  //       })
-  //         .then(user => {
-  //           res.statusCode = 200;
-  //           res.setHeader('Content-Type', 'application/json');
-  //           res.json({ status: 'Registration Successful!', user: user });
-  //         })
-  //         .catch(err => next(err));
-  //     }
-  //   })
-  //   .catch(err => next(err));
 
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, status: 'You are successfully logged in!' });
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 router.get('/logout', (req, res, next) => {
