d3a0c7ed3d2402769c3965d1b8308969d3ab5ee3
Marvin A <mka507@gmail.com>
2021-04-27 00:24:47 -0400

    File Upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
-
+const uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 
 // const url = 'mongodb://localhost:27017/nucampsite';
@@ -124,6 +124,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
